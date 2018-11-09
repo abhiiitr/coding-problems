9f16d5d7333a4210ae17dd737926ae28e5805fac
abhishek <sahu.abhishek.iitr@gmail.com>
2018-11-09 11:02:49 +0530

    Initial commit

---
+++ src/main/java/com/abhishek/tree/RootToLeafSum.java
@@ -0,0 +1,20 @@
+package com.abhishek.tree;
+
+public class RootToLeafSum {
+
+	int rootToLeafSum(){
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Node root = null;
+/*        root = bt.addNode(10, root);
+        root = bt.addNode(20, root);
+        root = bt.addNode(-10, root);
+        root = bt.addNode(15, root);
+        root = bt.addNode(0, root);
+        root = bt.addNode(21, root);
+        root = bt.addNode(-1, root);*/
+	}
+	
+}
